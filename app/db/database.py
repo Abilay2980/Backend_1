b03868f4c3b21cb19cb08e9b2d2a67554c25c096
Abilay <gnomik2984@gmail.com>
2026-04-14 16:10:51 +0500

    First working version

---
+++ app/db/database.py
@@ -0,0 +1,36 @@
+import asyncpg
+from app.core.config import settings
+
+
+class Database:
+    def __init__(self):
+        self.pool = None
+
+    async def connect(self):
+        self.pool = await asyncpg.create_pool(
+            user=settings.DB_USER,
+            password=settings.DB_PASS,
+            database=settings.DB_NAME,
+            host=settings.DB_HOST,
+            port=int(settings.DB_PORT),
+        )
+        print("Database is connected")
+
+    async def disconnect(self):
+        await self.pool.close()
+        print("Database is disconnected")
+
+    async def execute(self, query: str, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.execute(query, *args)
+
+    async def fetch(self, query: str, *args):
+        async with self.pool.acquire() as conn:
+            return await conn.fetch(query, *args)
+
+    async def get_db_connection(self):
+        async with self.pool.acquire() as conn:
+            yield conn
+
+db = Database()
+
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
-
+from contextlib import asynccontextmanager
 from app.api.endpoints.todo import todo_router
+from app.db.database import db
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    await db.connect()
+    yield
+    await db.disconnect()
 
 
-app = FastAPI()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(todo_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app")
+    uvicorn.run(app="main:app",reload =True)
